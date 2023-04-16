d698a38376996edd90715f3facee039ec52142db
roclimber <roclimber1@gmail.com>
2023-04-16 17:57:57 +0600

    add backend + docker settings

--- src/models/game.ts
+++ src/models/game.ts
@@ -154,7 +154,7 @@ class Game {
 
     public renderField(): void {
 
-        [-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9].map((index) => {
+        [-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9].forEach((index) => {
 
 
             const sign: number = this.getRandomSign()
